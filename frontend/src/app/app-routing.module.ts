06ca66df5ae68a4dc194ed34392d5fbb58f70314
kamych1916 <kamych1916@gmail.com>
2020-02-08 12:14:38 +0300

    add pages and components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**',  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, ru_RU } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
+
+/** config angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { AuthComponent } from './pages/auth/auth.component';
+import { RegistComponent } from './pages/regist/regist.component';
+
+import { RecoverComponent } from './pages/recover/recover.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './pages/main/header/header.component';
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RegistComponent,
+    RecoverComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ],
+  providers   : [
+    { provide: NZ_I18N, useValue: ru_RU }
+  ]
 })
 export class AppModule { }
