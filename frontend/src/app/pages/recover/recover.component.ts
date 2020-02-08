5b3d52f19859a31f08f0dbd4f276e9840e9138a4
kamych1916 <kamych1916@gmail.com>
2020-02-08 13:10:28 +0300

    add routing and new design-logic in pages

--- frontend/src/app/pages/recover/recover.component.ts
+++ frontend/src/app/pages/recover/recover.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+// import { User } from './interface';
+// import { HttpService } from './http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recover',
   templateUrl: './recover.component.html',
-  styleUrls: ['./recover.component.css']
+  styleUrls: ['./recover.component.css'],
+  // providers: [HttpService],  
 })
-export class RecoverComponent implements OnInit {
+export class RecoverComponent {
 
-  constructor() { }
+  stem="none"
+  // user: User = new User(); // данные от пользователя
+      
+  // constructor(private httpService: HttpService, public router: Router){}
 
-  ngOnInit() {
-  }
+  // receivedData: User; // полученные данные
+
+  
+  // sendDataUser(user: User){
+  //   if(user.Email == undefined){alert('email input value is empty!')}
+  //   else{
+  //   this.httpService.postData(user)
+  //     .subscribe(
+  //       (data: User) => {this.receivedData=data, alert('we send new password in your mail adress!'); this.router.navigate(['/auth'])},
+  //       error => { if(error.status == 500){ alert('invalid login')} else {alert('invalid password') }}
+  //     );
+  //   }
+  // };
 
 }
