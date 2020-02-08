4e59b210ec95794434e5087b944114a9e1c2a80a
serbud <bomberman192@yandex.ru>
2020-02-08 23:28:12 +0300

    add api for search tours. amin.

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,33 @@
 import aiohttp
 import asyncio
 import json
+from sanic.response import  json as jsons
+
+async def req3(query):
+    async with aiohttp.ClientSession() as session:
+        params = {
+                  'query': query,
+                  'lang': "ru",
+                  'lookFor': "both",
+                  'limit': 1
+                  }
+        async with session.get('https://engine.hotellook.com/api/v2/lookup.json', params=params) as resp:
+            print(resp.status)
+            d = json.loads(await resp.text())
+            d = d["results"]
+            # d = d[0]
+            d = d["locations"]
+            # print(json.dumps(d, indent=4))
+            return d
+
+    return {}
+
+
 
 async def req1(depart_start, return_start, origin_iata, destination_iata):
     async with aiohttp.ClientSession() as session:
+        # print("check_in", return_start)
+        # print("check_out", depart_start)
         params = {'origin_iata': origin_iata,#'MOW',
                   'destination_iata': destination_iata,#'TYO',
                   'depart_start': depart_start,#'2020-02-05',
@@ -15,35 +39,47 @@ async def req1(depart_start, return_start, origin_iata, destination_iata):
             print(resp.status)
             d = json.loads(await resp.text())
 
-            print(json.dumps(d, indent=4))
+            # print(json.dumps(d, indent=4))
+            return d
 
-    return res
+    return {}
 
 
-async def req2(check_in, check_out, adults, destination):
+async def req2(check_in, check_out, adults, ids):
+
+
+
+
     f = open("x.json", 'w')
-
+    res = []
+    # print("check_in",check_in)
+    # print("check_out",check_out)
     async with aiohttp.ClientSession() as session:
         data = {
             "page": "serp",
             "search_id": "",
             "params": {
                 "check_in": check_in,#"2020-02-15",
+                # "check_in": "2020-02-15",
+                # "check_out": "2020-02-16",
                 "check_out": check_out,#"2020-02-16",
                 "marker": "171596.70000000000.$1489",
                 "currency": "rub",
                 "locale": "ru",
                 "rooms": [
                     {
-                        "adults": adults,#1,
+                        # "adults": 1,
+                        "adults": int(adults),#1,
                         "children": []
                     }
                 ],
                 "locations_ids": [
-                    2764
+                    # 2764
+                    int(ids)
                 ],
                 "hotels_ids": [],
-                "destination": destination,#"Стамбул, Турция ",
+                # "destination": "Стамбул, Турция ",
+                "destination": "null",
                 "host": "s.travel.megafon.ru",
                 "flags": {
                     "auid": "null",
@@ -108,31 +144,49 @@ async def req2(check_in, check_out, adults, destination):
             "limit": 10,
             "offset": 0
         }
-        data = json.dumps(data).encode()
-        print(data)
+        # data = json.dumps(data).encode()
+        # print(data)
         async with session.post('https://s.travel.megafon.ru/api/wl_search/result', json=data) as resp:
             print(resp.status)
             d = json.loads(await resp.text())
+            # loc = d[ids]
+            hotels = d["hotels"]
+            # return d
+            # hotels_amenties = d["filters"]
+            # hotels_amenties = hotels_amenties["counters"]
+            # hotels_amenties = hotels_amenties["prices"]
+            # hotels_amenties = hotels_amenties["property_types"]
+            hotels_amenties = d["hotels_amenities"]
+
             print(json.dumps(d, indent =4))
-            search_id = d["search_id"]
+            # search_id = d["search_id"]
             loc = []
-            for key in d["locations"]:
-                loc.append(key)
+            # print("locations" in d)
+            print("locations")
+            # for key in d["locations"].keys():
+            #     print(key)
+            #     # print(type(key))
+                # if key in d:
+                #     k = d[key]
+                #
+                #     print(k["hotels"])
+            # stop = d["stop"]
+            # f.write(json.dumps(d, ))
 
 
-            stop = d["stop"]
-            f.write(json.dumps(d, ))
+
+        # print(type(stop))
+        for i in range(3):
 
 
-
-        print(type(stop))
-        for i in range(1):
-            if stop != False:
+            if not ("stop" in d) or (d["stop"] != False):
                 break
+            # if stop != False:
+            #     break
             async with aiohttp.ClientSession() as session:
                 data = {
                         "page": "serp",
-                        "search_id": search_id,
+                        "search_id": "",
                         "params": {
                             "check_in": check_in,#"2020-03-29",
                             "check_out": check_out,#"2020-03-31",
@@ -141,11 +195,11 @@ async def req2(check_in, check_out, adults, destination):
                             "locale": "ru",
                             "rooms": [
                                 {
-                                    "adults": adults,#2,
+                                    "adults": int(adults),#2,
                                     "children": []
                                 }
                             ],
-                            "locations_ids": loc,
+                            "locations_ids": [int(ids)],
                             "destination": 'null',
                             "hotels_ids": [],
                             "host": "s.travel.megafon.ru",
@@ -209,24 +263,36 @@ async def req2(check_in, check_out, adults, destination):
                             }
                         },
                         "sort": "popularity",
-                        "limit": 100,
+                        "limit": 10,
                         "offset": 0
                     }
 
                 async with session.post('https://s.travel.megafon.ru/api/wl_search/result', json=data) as resp:
                     print(resp.status)
                     d = json.loads(await resp.text())
-                    if not "stop" in d:
-                        break
-                    stop = d["stop"]
+                    # if not "stop" in d:
+                    #     break
+                    for key in d:
+                        print(key)
+
+                    if "hotels" in d:
+                        hotels.extend(d["hotels"])
+                    # stop = d["stop"]
                     f.write(json.dumps(d))
                     # print(stop)
-                    print(json.dumps(d, indent=4).encode())
+                    # print(json.dumps(d, indent=4).encode())
+    return hotels, hotels_amenties
 
-def a1(depart_start, return_start, origin_iata, destination_iata):
-    return asyncio.run(req1(depart_start, return_start, origin_iata, destination_iata))
-def a2(check_in, check_out, adults, destination):
-    return asyncio.run(req2(check_in, check_out, adults, destination))
+    return {"ss":"ss"}
+async def a1(depart_start, return_start, origin_iata, destination_iata):
+    return await req1(depart_start, return_start, origin_iata, destination_iata)
 
-f = asyncio.run(req1("2020-02-15", "2020-02-16", origin_iata, destination_iata))
-print(f)
+async def a2(check_in, check_out, adults, destination):
+    return await req2(check_in, check_out, adults, destination)
+    # event_loop = asyncio.new_event_loop()
+    # event_loop.run_until_complete(req2(check_in, check_out, adults, destination))
+
+async def a3(query):
+    return await req3(query)
+# f = asyncio.run(req1("2020-02-15", "2020-02-16", origin_iata, destination_iata))
+# print(f)
