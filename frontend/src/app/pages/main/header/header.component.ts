776f8dc2cc4cf7fdc2172f465bd5142fa1c14c13
kamych1916 <kamych1916@gmail.com>
2020-02-08 15:59:57 +0300

    add header component and design-logic in main page

--- frontend/src/app/pages/main/header/header.component.ts
+++ frontend/src/app/pages/main/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  hamburger: boolean= false;
+  buttonsonheader: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+    if(window.innerWidth <= 768){
+      this.hamburger = true
+      this.buttonsonheader = false
+    }else{
+      this.hamburger = false
+      this.buttonsonheader = true
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+      if(event.target.innerWidth <= 768){
+        this.hamburger = true
+        this.buttonsonheader = false
+      }else{
+        this.hamburger = false
+        this.buttonsonheader = true
+      }
   }
 
 }
--- frontend/src/app/pages/main/main.component.ts
+++ frontend/src/app/pages/main/main.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  CountHuman = (value: number) => `${value} человека`;
+  CountDays = (value: number) => `${value} ночей`;
+  
+  selectedCity = null;
+  selectedCountry = null;
+
+  ValueHuman = 2;
+  ValueDays = 5;
+
+  openModal: boolean=false;
+
+  isCollapsed = false;
+  
+  isLoadingOne = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  ModalEvent(type){
+    this.isLoadingOne = true;
+    if(type == 1){
+      setTimeout(() => {
+        this.isLoadingOne = false;
+        this.openModal = true
+      }, 2000);
+    }else{
+      this.openModal = false
+      this.isLoadingOne = false;
+    }
+  }
+  
 
 }
