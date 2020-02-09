b89b3c6d6843930332484f57e99fcc683c91812e
Pavel Katskov <katskov.p@gmail.com>
2020-02-09 11:03:00 +0300

    map worded

--- frontend/src/app/pages/map/map.component.ts
+++ frontend/src/app/pages/map/map.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 import * as L from "leaflet";
-let countriesGeoJson = require('./countries.geojson');
-countriesGeoJson = JSON.parse(countriesGeoJson);
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,19 +8,57 @@ countriesGeoJson = JSON.parse(countriesGeoJson);
 })
 // import countries_jeoJson
 export class MapComponent implements OnInit {
-
-  constructor() { }
+  map: L.Map;
+  json;
+  options = {
+    layers: [
+      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 5,
+        attribution: ""
+      })
+    ],
+    zoom: 2,
+    center: L.latLng(47.482019, -1)
+  };
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
-  options = {
-    layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      L.geoJson(countriesGeoJson)
-    ],
-    zoom: 5,
-    center: L.latLng(46.879966, -121.726909)
-  };
+  onMapReady(map: L.Map) {
+    this.http.get("http://cdn.katskov.ru/countries.geojson").subscribe((json: any) => {
+      console.log(json);
+      this.json = json;
+      function getColor(properties) {
+        if ("filters" in properties){
+          console.log(properties.filters[0])
+          if (properties.filters[0] == "1"){
+            return "#944";
+          }
+          if (properties.filters[0] == "2"){
+            return "#494";
+          }
+          if (properties.filters[0] == "3"){
+            return "#449";
+          }
+          
+        }
+        return "#999";
+      }
+      function style(feature) {
+        return {
+            fillColor: getColor(feature.properties),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    };
+      L.geoJSON(this.json, {style: style}).addTo(map);
+    });
+  }
+
+  
 
 }
