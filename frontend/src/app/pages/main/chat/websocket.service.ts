dadaf7500664ffc1afc7b24a2a099d6a3ace2454
Pavel Katskov <katskov.p@gmail.com>
2020-02-09 12:14:58 +0300

    add slider

--- frontend/src/app/pages/main/chat/websocket.service.ts
+++ frontend/src/app/pages/main/chat/websocket.service.ts
@@ -1,7 +1,7 @@
 import { Observable, Subject } from 'rxjs';
 import * as io from 'socket.io-client';
-// let url = 'http://127.0.0.1:8000';
-let url = 'http://192.168.43.67:8000'
+let url = 'http://127.0.0.1:8000';
+// let url = 'http://192.168.43.67:8000'
 const socketio = io(url)
 
 export class WebsocketService {
